1e4f0b0b5f1ac04d3fd9ee9d6e4fe2613a6efc07
Jean <adiariakus@gmail.com>
2024-10-29 13:40:59 -0500

    Play game

--- script.js
+++ script.js
@@ -13,23 +13,41 @@ const getComputerChoice = () => {
 const getHumanChoice = () => {
     let choice;
     do {
-        choice = prompt("rock, paper or scissors?: ").toLowerCase();
+        choice = prompt("rock, paper or scissors?: ");
+        choice = choice.toLowerCase();
     } while(choice !== "rock" && choice !== "scissors" && choice !== "paper");
+    return choice;
 }
 
-let computerScore = 0;
-let humanScore = 0;
+const playGame = () => {
+    let computerScore = 0;
+    let humanScore = 0;
 
-const playRound = (humanChoice, computerChoice) => {
-    if (humanChoice === computerChoice){
-        return "It's a draw";
-    } else if ((humanChoice === "paper" && computerChoice ===  "rock")
-        || (humanChoice === "scissors" && computerChoice === "paper")
+    const playRound = (humanChoice, computerChoice) => {
+        if (humanChoice === computerChoice){
+            return "It's a draw";
+        } else if ((humanChoice === "paper" && computerChoice ===  "rock")
+            || (humanChoice === "scissors" && computerChoice === "paper")
             || (humanChoice === "rock" && computerChoice === "scissors")){
-        humanScore++;
-        return "Human wins!";
+            humanScore++;
+            return "Human wins!";
+        } else {
+            computerScore++;
+            return "Computer wins!"
+        }
+    }
+
+    while(computerScore < 5 && humanScore < 5){
+        const computerChoice = getComputerChoice();
+        const humanChoice = getHumanChoice();
+        const result = playRound(humanChoice, computerChoice);
+        console.log(result);
+    }
+    if (computerScore === 5) {
+        console.log("Computer wins the game!!");
     } else {
-        computerScore++;
-        return "Computer wins!"
+        console.log("Human wins the game!!");
     }
 }
+
+playGame();
