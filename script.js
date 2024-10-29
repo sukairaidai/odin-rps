b949ffabde4a8d20fcacfe56edc676814d247087
Jean <adiariakus@gmail.com>
2024-10-29 13:28:46 -0500

    Play round

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ const getHumanChoice = () => {
 
 let computerScore = 0;
 let humanScore = 0;
+
+const playRound = (humanChoice, computerChoice) => {
+    if (humanChoice === computerChoice){
+        return "It's a draw";
+    } else if ((humanChoice === "paper" && computerChoice ===  "rock")
+        || (humanChoice === "scissors" && computerChoice === "paper")
+            || (humanChoice === "rock" && computerChoice === "scissors")){
+        humanScore++;
+        return "Human wins!";
+    } else {
+        computerScore++;
+        return "Computer wins!"
+    }
+}
