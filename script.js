7ad36a6112aa725cec0b53fd02907d06d559c8ce
Jean <adiariakus@gmail.com>
2024-10-30 21:01:30 -0500

    Create functionless UI

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber === 0){
+    if (randomNumber === 0) {
         return "rock";
-    } else if (randomNumber === 1){
+    } else if (randomNumber === 1) {
         return "paper";
     } else {
         return "scissors";
@@ -15,7 +15,7 @@ const getHumanChoice = () => {
     do {
         choice = prompt("rock, paper or scissors?: ");
         choice = choice.toLowerCase();
-    } while(choice !== "rock" && choice !== "scissors" && choice !== "paper");
+    } while (choice !== "rock" && choice !== "scissors" && choice !== "paper");
     return choice;
 }
 
@@ -24,11 +24,11 @@ const playGame = () => {
     let humanScore = 0;
 
     const playRound = (humanChoice, computerChoice) => {
-        if (humanChoice === computerChoice){
+        if (humanChoice === computerChoice) {
             return "It's a draw";
-        } else if ((humanChoice === "paper" && computerChoice ===  "rock")
+        } else if ((humanChoice === "paper" && computerChoice === "rock")
             || (humanChoice === "scissors" && computerChoice === "paper")
-            || (humanChoice === "rock" && computerChoice === "scissors")){
+            || (humanChoice === "rock" && computerChoice === "scissors")) {
             humanScore++;
             return "Human wins!";
         } else {
@@ -37,7 +37,7 @@ const playGame = () => {
         }
     }
 
-    while(computerScore < 5 && humanScore < 5){
+    while (computerScore < 5 && humanScore < 5) {
         const computerChoice = getComputerChoice();
         const humanChoice = getHumanChoice();
         const result = playRound(humanChoice, computerChoice);
@@ -50,4 +50,29 @@ const playGame = () => {
     }
 }
 
-playGame();
+
+const body = document.querySelector("body");
+const main = document.createElement("main");
+body.append(main);
+
+const createDisplay = () => {
+    const display = document.createElement("p");
+    display.classList.add("display");
+    main.append(display);
+}
+
+const createChoiceButtons = () => {
+    const menu = document.createElement("div");
+    menu.classList.add("menu");
+    const choices = ["rock", "paper", "scissors"];
+    for (let choice of choices) {
+        const button = document.createElement("button");
+        button.textContent = choice;
+        button.classList.add("choice");
+        menu.appendChild(button);
+    }
+    main.append(menu);
+}
+
+createDisplay();
+createChoiceButtons();
